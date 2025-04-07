f75776d25e22916792d45a85811908be6b9a6d6d
Sneha Singh <127472458+Sneha-Singh21@users.noreply.github.com>
2025-04-07 15:50:40 +0530

    Update firebase.js

--- nextjs-firebase-auth/src/lib/firebase.js
+++ nextjs-firebase-auth/src/lib/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-// Your web app's Firebase configuration
+// web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -12,7 +12,7 @@ const firebaseConfig = {
   };
   
 
-// Initialize Firebase
+// Initializing Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
